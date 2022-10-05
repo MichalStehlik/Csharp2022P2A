bdc04428f6f09a167cb842aa8e0e2e14e6ac2c92
Michal Stehlik <michal.stehlik@pslib.cz>
2022-10-05 09:39:43 +0200

    enums

---
+++ Cst03enum/Program.cs
@@ -0,0 +1,31 @@
+﻿// Enum
+
+Color bg = Color.White;
+Console.WriteLine(bg);
+Console.WriteLine((int)bg);
+
+// Operatory
+// unarni
+// !x
+// binarni
+// a + b
+// ternarni
+int a = 0;
+int c = 4;
+Console.WriteLine(a == 0 ? "Nic" : "Neco");
+int b = 12 + --c + ((a == 0) ? 2 : 4);
+public enum Color {
+    Red,
+    Green,
+    Blue,
+    White,
+    Black
+}
+
+public enum Gender
+{
+    Unknown = 0,
+    Male = 1,
+    Female = 2,
+    Other = 3
+}

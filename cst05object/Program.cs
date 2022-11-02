886f822d63ae296052a659ac12014364ff8263bb
MichalStehlik <michal.stehlik@pslib.cz>
2022-11-02 09:40:40 +0100

    objects

---
+++ cst05object/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using cst05object;
+
+//Student adam = new Student();
+Student adam = new Student("Adam","Kos",2,1.3);
+Student fred = new Student();
+//adam.firstname = "Adam";
+//Console.WriteLine(adam.firstname);
+//Console.WriteLine(adam.lastname);
+//Console.WriteLine(adam);
+//adam.firstname = "Adolf";
+//Console.WriteLine(adam.firstname);
+Console.WriteLine(adam.GetFirstname());
+Console.WriteLine(adam.GetGrade());
+adam.Advance();
+adam.Advance();
+adam.Advance();
+adam.Advance();
+adam.SetGrade(-12);
+Console.WriteLine(adam.GetGrade());
+Console.WriteLine(fred.GetFirstname());
---
+++ cst05object/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cst05object
+{
+    internal class Student
+    {
+        private string firstname = "Fred";
+        private string lastname;
+        private int grade = 1;
+        private double averageMark = 3;
+
+        public Student(string firstname, string lastname, int grade, double averageMark)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.grade = grade;
+            this.averageMark = averageMark;
+        }
+
+        public Student()
+        {
+            lastname = "Vopička";
+        }
+
+        public string GetFirstname()
+        {
+            return this.firstname;
+        }
+        public int GetGrade()
+        {
+            return grade;
+        }
+
+        public int Advance()
+        {
+            if (grade < 4)
+            {
+                this.grade++;
+            }     
+            return this.grade;
+        }
+
+        public void SetGrade(int value)
+        {
+            if (value > 0 && value <= 4)
+                this.grade = value;
+        }
+    }
+}

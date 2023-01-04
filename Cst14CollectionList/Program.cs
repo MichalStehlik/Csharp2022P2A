01e3f4fdd3b7061162ae4ac2b329f79b84339234
MichalStehlik <michal.stehlik@pslib.cz>
2023-01-04 09:32:40 +0100

    arraylist vs list

---
+++ Cst14CollectionList/Program.cs
@@ -0,0 +1,33 @@
+﻿using Cst14CollectionList;
+using System.Collections;
+
+ArrayList stupidList = new ArrayList();
+stupidList.Add(4);
+stupidList.Add("Ahoj");
+stupidList.Add(new Student { Firstname = "Adam"});
+stupidList.Add(new Student { Firstname = "Bořivoj" });
+stupidList.Add(new Student { Firstname = "Cyril" });
+Console.WriteLine(stupidList[0]);
+foreach (var x in stupidList)
+{
+    Console.WriteLine(x);
+}
+foreach (var x in stupidList)
+{
+    if (x is Student)
+    {
+        Console.WriteLine((x as Student).Firstname);
+    }  
+}
+
+Console.WriteLine("----");
+List<Student> students = new List<Student>();
+students.Add(new Student { Firstname = "Adam" });
+students.Add(new Student { Firstname = "Bořivoj" });
+students.Add(new Student { Firstname = "Cyril" });
+foreach(Student x in students)
+{
+    Console.WriteLine(x.Firstname);
+}
+Console.WriteLine(students[0].Firstname);
+// Console.WriteLine(students[100].Firstname); // exception

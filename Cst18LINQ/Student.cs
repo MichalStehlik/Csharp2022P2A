0cb8bbfdca6a699e947c4f10a6b57f78818599ad
MichalStehlik <michal.stehlik@pslib.cz>
2023-02-01 09:08:08 +0100

    linq start

---
+++ Cst18LINQ/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cst18LINQ
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        public string Classroom { get; set; }
+        public double AverageGrade { get; set; }
+        public override string ToString()
+        {
+            return FirstName + " " + Classroom + ":" + AverageGrade;
+        }
+    }
+}
